0b4909d544ca0600461bba1d1890242246806b7f
amaan <amanmulla231@gmail.com>
2025-11-30 10:12:46 +0530

    inital

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,12 +15,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import logout
 from django.urls import path, include
 from users import views as user_views
+
+# Custom LogoutView that accepts GET requests
+class LogoutView(auth_views.LogoutView):
+    http_method_names = ['get', 'post', 'options']
+    
+    def dispatch(self, request, *args, **kwargs):
+        # Handle both GET and POST the same way
+        if request.method.lower() == 'get':
+            # For GET requests, logout and render template
+            logout(request)
+            from django.template.response import TemplateResponse
+            return TemplateResponse(request, self.template_name, {})
+        # For POST requests, use parent's implementation
+        return super().dispatch(request, *args, **kwargs)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls'),)
 ]
  
