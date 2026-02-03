a59c1c00b231c8feed55adc6584048c4c6092a77
amaan <amanmulla231@gmail.com>
2026-02-03 10:59:10 +0530

    fixed the default img

--- users/models.py
+++ users/models.py
@@ -4,7 +4,15 @@ from PIL import Image
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(
+        upload_to='profile_pics/',
+        default='profile_pics/default.jpg'
+    )
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics/')
 
     def __str__(self):
         return f'{self.user.username} Profile'
